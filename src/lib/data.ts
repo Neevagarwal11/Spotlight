79a48c6a5dc141ac9a486cca11ff1de7f35a206f
Neev Agarwal <agarwalneev2005@gmail.com>
2026-03-22 13:05:46 +0530

    Home Dev Done

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,4 @@
+import { CallStatusEnum } from "@prisma/client";
 import { HomeIcon, Network, SettingsIcon, Sparkle, Sparkles, Webcam } from "lucide-react";
 
 export const sidebarData = [
@@ -37,4 +38,49 @@ export const OnBoardingSteps = [
     {id:1, title: 'Create a webinar', complete: false , link:''},
     {id:2, title: 'Get leads', complete: false , link:''},
     {id:3, title: 'Conversion status', complete: false , link:''},
+]
+
+export const potentialCustomers = [
+    {
+        id:"1",
+        name:"John Doe",
+        email:"johndoe@example.com",
+        clerkId:"1",
+        profileImage:"/vercel.svg",
+        isActive:true,
+        lastLoginAt: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt:null,
+        tags:["New" , "Hot Lead"],
+        callStatus: CallStatusEnum.COMPLETED
+    },
+    {
+        id:"2",
+        name:"John Doe",
+        email:"johndoe@example.com",
+        clerkId:"1",
+        profileImage:"/vercel.svg",
+        isActive:true,
+        lastLoginAt: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt:null,
+        tags:["New" , "Hot Lead"],
+        callStatus: CallStatusEnum.COMPLETED
+    },
+    {
+        id:"3",
+        name:"John Doe",
+        email:"johndoe@example.com",
+        clerkId:"1",
+        profileImage:"/vercel.svg",
+        isActive:true,
+        lastLoginAt: null,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt:null,
+        tags:["New" , "Hot Lead"],
+        callStatus: CallStatusEnum.COMPLETED
+    },
 ]
